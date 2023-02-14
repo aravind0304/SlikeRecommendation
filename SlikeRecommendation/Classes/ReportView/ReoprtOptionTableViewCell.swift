c8ab75b0204b21e69a39ba773f3bf4997f335f5d
Aravind Kumar <aravind.kumar@timesinternet.in>
2023-02-14 13:07:15 +0530

    Report View

---
+++ SlikeRecommendation/Classes/ReportView/ReoprtOptionTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ReoprtOptionTableViewCell.swift
+//  SlikeRecommendation
+//
+//  Created by Aravind Kumar on 14/02/23.
+//
+
+import UIKit
+
+class ReoprtOptionTableViewCell: UITableViewCell {
+    @IBOutlet weak var line: UIImageView!
+    
+    @IBOutlet weak var radioBtn: UIImageView!
+    
+    @IBOutlet weak var lblHint: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SlikeRecommendation/Classes/ReportView/RoprtListViewController.swift
+++ SlikeRecommendation/Classes/ReportView/RoprtListViewController.swift
@@ -1,39 +1,38 @@
 //
-//  ReportViewController.swift
+//  RoprtListViewController.swift
 //  SlikeRecommendation
 //
-//  Created by Aravind Kumar on 07/02/23.
+//  Created by Aravind Kumar on 14/02/23.
 //
 
 import UIKit
 
-class ReportViewController: UIViewController {
+class RoprtListViewController: UIViewController {
     @IBOutlet weak var btnClose: UIButton!
-    private let kReportHeaderTableViewCell = "ReportHeaderTableViewCell"
+
+    private let kReoprtOptionTableViewCell = "ReoprtOptionTableViewCell"
     @IBOutlet weak var tbView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         self.registerNib()
         self.tbView.delegate = self
         self.tbView.dataSource = self
         self.btnClose.setImage(RecBundleManager.image(named: "sl_close"), for: .normal)
         self.btnClose.setImage(RecBundleManager.image(named: "sl_close"), for: .selected)
-
+        // Do any additional setup after loading the view.
     }
     func registerNib() {
         
-        let nibView = UINib(nibName: "ReportHeaderTableViewCell", bundle: RecBundleManager.resourcesBundle())
+        let nibView = UINib(nibName: kReoprtOptionTableViewCell, bundle: RecBundleManager.resourcesBundle())
         print(nibView)
-        self.tbView.register(nibView, forCellReuseIdentifier: kReportHeaderTableViewCell)
+        self.tbView.register(nibView, forCellReuseIdentifier: kReoprtOptionTableViewCell)
     }
     @IBAction func closeAction(_ sender: Any) {
         self.dismiss(animated: true)
     }
+    
 }
-extension ReportViewController:UITableViewDelegate,UITableViewDataSource {
+extension RoprtListViewController:UITableViewDelegate,UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -55,39 +54,21 @@ extension ReportViewController:UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 900
-        }
-        return 0
+        return 50
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
                 
-        guard let cell : ReportHeaderTableViewCell = tableView.dequeueReusableCell(withIdentifier: kReportHeaderTableViewCell, for: indexPath) as? ReportHeaderTableViewCell else {
+        guard let cell : ReoprtOptionTableViewCell = tableView.dequeueReusableCell(withIdentifier: kReoprtOptionTableViewCell, for: indexPath) as? ReoprtOptionTableViewCell else {
             fatalError("Unable to Dequeue Reusable Table View Cell")
         }
         cell.backgroundColor = UIColor.clear
-        cell.delegate = self
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-    }
-}
-extension ReportViewController : SelctActionType {
-    func selctValue(vaule: Int) {
-        if vaule == 11 {
-            //Open List
-            //RoprtListViewController
-            print("Aravind")
-            //
-            let roprtListViewController = RoprtListViewController(nibName: "RoprtListViewController", bundle: RecBundleManager.resourcesBundle())
-            roprtListViewController.modalPresentationStyle = .fullScreen
-            self.present(roprtListViewController, animated: true)
-        }
     }
 }
