570f1b00ffe19adcecc9550b4df5d43f4f2fa98d
Aravind Kumar <aravind.kumar@timesinternet.in>
2025-08-05 12:29:22 +0530

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,36 @@
+//
+//  Package.swift
+//  Pods
+//
+//  Created by Aravind Kumar on 05/08/25.
+//
+
+
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "SlikeRecommendation",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SlikeRecommendation",
+            targets: ["SlikeRecommendation"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.2.0")
+    ],
+    targets: [
+        .target(
+            name: "SlikeRecommendation",
+            dependencies: ["AlamofireImage"],
+            path: "SlikeRecommendation/Classes",
+            resources: [
+                .process("../SlikeRecResources")
+            ]
+        )
+    ]
+)
